9070bb7a79e754a7ee0d0c03e46d5110ded40d78
zhenggang <mrzhenggang@foxmail.com>
2021-11-05 00:49:57 +0800

    来自郑刚的手动部署

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -ex
+git config --global http.sslVerify "false"
+
+# 生成静态文件
+hexo clean && hexo g 
+echo "blog.mrzhenggang.com" > public/CNAME
+
+# 推送本站
+# 配置git
+git config --global user.name "zhenggang"
+git config --global user.email "mrzhenggang@foxmail.com" 
+
+# deploy nscc.git
+msg='来自郑刚的手动部署'
+git add -A
+git commit -m "$msg"
+git push -u origin main
+
+# 推送public内容
+hexo d
+
+# clean
+hexo cleant config --global http.sslVerify "false"
+
+# 生成静态文件
+hexo clean && hexo g 
+echo "blog.mrzhenggang.com" > public/CNAME
+
+# 推送本站
+# 配置git
+git config --global user.name "zhenggang"
+git config --global user.email "mrzhenggang@foxmail.com" 
+
+# deploy nscc.git
+msg='来自郑刚的手动部署'
+git add -A
+git commit -m "$msg"
+git push -u origin main
+
+# 推送public内容
+hexo d
+
+# clean
+hexo clean
+
+
---
+++ source/categories/index.md
@@ -0,0 +1,5 @@
+---
+title: 文章分类
+type: "categories"
+date: 2021-11-05 00:28:39
+---
