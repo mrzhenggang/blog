3cc9a82b18b7140f3ddd3886d95a1cd14ad8e753
zhenggang <mrzhenggang@foxmail.com>
2021-11-05 00:51:12 +0800

    来自郑刚的手动部署

--- deploy.sh
+++ deploy.sh
@@ -21,27 +21,4 @@ git push -u origin main
 hexo d
 
 # clean
-hexo cleant config --global http.sslVerify "false"
-
-# 生成静态文件
-hexo clean && hexo g 
-echo "blog.mrzhenggang.com" > public/CNAME
-
-# 推送本站
-# 配置git
-git config --global user.name "zhenggang"
-git config --global user.email "mrzhenggang@foxmail.com" 
-
-# deploy nscc.git
-msg='来自郑刚的手动部署'
-git add -A
-git commit -m "$msg"
-git push -u origin main
-
-# 推送public内容
-hexo d
-
-# clean
-hexo clean
-
-
+hexo cleant
